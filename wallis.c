dd2020426a37d35cc54122592f2a4a4807b93f48
Adithyan227 <84662854+Adithyan227@users.noreply.github.com>
2021-06-02 17:37:06 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,4 +23,13 @@ int main(void) {
     }
   }
 }
+float wallis_pi(int n)
+{
+ float pi_value;
+ pi_value=1;
+ for(int y=1;y<=n;y++)
+  pi_value*=((4*y*y)/((4*y*y)-1));
+ pi_value+=2;
+ return pi_value;
+}
 
